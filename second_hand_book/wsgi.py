412025b818a4cb1d0af61e76fa6ede6ab26702a0
chrisq557 <chrisq557santos@gmail.com>
2025-12-08 22:27:40 +0000

    Log DJANGO_SETTINGS_MODULE during WSGI boot for Heroku diagnostics

--- second_hand_book/wsgi.py
+++ second_hand_book/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'second_hand_book.settings')
 
+# Diagnostic: print which settings module is used at runtime (appears in Heroku logs)
+try:
+    import sys
+    print("DJANGO_SETTINGS_MODULE:", os.environ.get("DJANGO_SETTINGS_MODULE"), file=sys.stderr)
+except Exception:
+    pass
+
 application = get_wsgi_application()
